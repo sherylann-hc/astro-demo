be7978fcfb09740bf5366c00a46d59de981de138
sherylann-hc <sheryl@hashicorp.com>
2023-03-21 19:43:11 +0800

    TF Code for Demo

---
+++ tfcode/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+####Provisioning resources direct
+resource "aws_s3_bucket" "astro-demo" {
+  bucket = var.s3_bucket_name
+  acl    = var.s3_bucket_acl
+
+  versioning {
+    enabled = true
+  }
+}
+
+####Provisioning via modules
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
+
+#   bucket = var.s3_bucket_name
+#   acl    = var.s3_bucket_acl
+
+#   versioning = {
+#     enabled = true
+#   }
+# }
---
+++ tfcode/variables.tf
@@ -0,0 +1,10 @@
+variable "s3_bucket_acl" {
+  description = "S3 Bucket ACL"
+  type        = string
+  default = "private"
+}
+
+variable "s3_bucket_name" {
+  description = "S3 Bucket Name"
+  type        = string
+}
