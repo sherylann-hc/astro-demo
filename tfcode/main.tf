4b31344aaa69a3daf36283a456dd0c138c957952
sherylann-hc <sheryl@hashicorp.com>
2023-03-22 10:46:54 +0800

    updated code

--- tfcode/main.tf
+++ tfcode/main.tf
@@ -15,6 +15,7 @@ provider "aws" {
 ####Provisioning via modules
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 2.0"
 
   bucket = var.s3_bucket_name
   acl    = var.acl
