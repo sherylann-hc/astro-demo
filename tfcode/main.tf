9bccaca2e7e407de53de8c0648ff2c412e32c645
sherylann-hc <sheryl@hashicorp.com>
2023-03-24 16:46:37 +0800

    provision via direct resource code

--- tfcode/main.tf
+++ tfcode/main.tf
@@ -3,24 +3,24 @@ provider "aws" {
 }
 
 ####Provisioning resources direct
-# resource "aws_s3_bucket" "astro-demo" {
-#   bucket = var.s3_bucket_name
-#   acl    = var.acl
-
-#   versioning {
-#     enabled = true
-#   }
-# }
-
-####Provisioning via modules
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 2.0"
-
+resource "aws_s3_bucket" "astro-demo" {
   bucket = var.s3_bucket_name
   acl    = var.acl
 
-  versioning = {
+  versioning {
     enabled = true
   }
 }
+
+####Provisioning via modules
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
+#   version = "~> 2.0"
+
+#   bucket = var.s3_bucket_name
+#   acl    = var.acl
+
+#   versioning = {
+#     enabled = true
+#   }
+# }
